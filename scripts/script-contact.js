cb8634489141f1454ca682b0ac5a875aabea486b
Hector Orozco <hectorjoseorozco@gmail.com>
2021-08-30 15:10:11 -0500

    Add SmtpJS.com script code to script-contact.js

--- scripts/script-contact.js
+++ scripts/script-contact.js
@@ -90,3 +90,25 @@ const setEmailBody = () => {
 
 // When the page loads
 document.getElementById("send-button").addEventListener("click", sendEmail);
+
+// SmtpJS.com - v3.0.0 used to send an email//
+var Email = {
+    send: function (a) {
+        return new Promise(function (n, e) {
+            a.nocache = Math.floor(1e6 * Math.random() + 1), a.Action = "Send";
+            var t = JSON.stringify(a);
+            Email.ajaxPost("https://smtpjs.com/v3/smtpjs.aspx?", t, function (e) { n(e) })
+        })
+    },
+    ajaxPost: function (e, n, t) {
+        var a = Email.createCORSRequest("POST", e);
+        a.setRequestHeader("Content-type", "application/x-www-form-urlencoded"), a.onload =
+            function () { var e = a.responseText; null != t && t(e) }, a.send(n)
+    },
+    createCORSRequest: function (e, n) {
+        var t = new XMLHttpRequest;
+        return "withCredentials" in t ?
+            t.open(e, n, !0) :
+            "undefined" != typeof XDomainRequest ? (t = new XDomainRequest).open(e, n) : t = null, t
+    }
+};
